419928dbcf375ada5f1040204ff856cd8cabe12b
Shivam-0103 <128032009+Shivam-0103@users.noreply.github.com>
2023-03-18 14:26:48 +0530

    Update Assignment3.c

--- Assignment3.c
+++ Assignment3.c
@@ -3,5 +3,19 @@
 
 int main()
 {
+  int dec, rem, binary = 0, base = 1;
+
+   printf("Enter a decimal number: ");
+   scanf("%d", &dec);
+
+   while (dec> 0)
+    {
+      rem= dec% 2;
+      binary = binary + rem * base;
+      dec = dec / 2;
+      base = base * 10;
+   }
+
+   printf("Binary equivalent: %d", binary);
   return 0;
 }
