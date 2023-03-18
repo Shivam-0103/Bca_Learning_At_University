700e82688f54e0afe3a35811ea3b6fbe6aedbd13
Shivam-0103 <128032009+Shivam-0103@users.noreply.github.com>
2023-03-18 13:42:05 +0530

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -2,6 +2,26 @@
 
 int main()
 {
-  //add your code here
+   int i,n,num,pos=0,neg=0;
+ 
+  printf("please enter the numbers:");
+   scanf("%d",&n);
+ 
+ for(i=1;i<=n;i++)
+ {
+ 	printf("enter the number:%d\n",i);
+ 	scanf("%d",&num);
+ 	if(num>0)
+ 	{
+ 		pos++;
+ 		
+	 }
+	 else if(num<0){
+	 	neg++;
+	 }
+	
+ }
+  printf("the total number of positive are: %d\n",pos);
+	 printf("the total number of negative are: %d",neg);
   return 0;
 }
