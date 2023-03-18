f0be6fd1fccd03a1bcc7a5ed832a6050a1ed5c05
Shivam-0103 <128032009+Shivam-0103@users.noreply.github.com>
2023-03-18 16:24:58 +0530

    Update Assignment5.c

--- Assignment5.c
+++ Assignment5.c
@@ -4,5 +4,46 @@
 
 int main()
 {
+  	int scores[50]; // assume max number of students in class is 50
+    int i, n, max_score;
+
+    // Input the scores
+    printf("Enter the number of students in the class (up to 50): ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("Enter the score of student %d: ", i);
+        scanf("%d", &scores[i]);
+    }
+
+    // Compute the maximum score
+    max_score = scores[1];
+    for (i = 1; i <n; i++) {
+        if (scores[i] > max_score) {
+            max_score = scores[i];
+        }
+    }
+    printf("The maximum score in the class is: %d\n", max_score);
+
+    // Determine which students have passed and which have failed
+    printf("The following students have passed:\n");
+    for (i = 1; i <= n; i++) {
+        if (scores[i] >= 50 && scores[i] != -1) {
+            printf("Student %d\n", i);
+        }
+ }
+    printf("The following students have failed:\n");
+    for (i = 1; i <= n; i++) {
+        if (scores[i] < 50 && scores[i] != -1) {
+            printf("Student %d\n", i);
+        }
+    }
+  printf("the following students wre absent:\n");
+  for(i=1;i<=n;i++)
+  {
+   if(scores[i]==-1){
+   	printf("student %d\n",i);
+   }
+  }
   return 0;
 }
