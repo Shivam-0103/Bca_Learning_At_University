da46643c7f44b5626c6f92fae227b029bfb0be17
Shivam-0103 <128032009+Shivam-0103@users.noreply.github.com>
2023-03-18 20:59:48 +0530

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -2,6 +2,45 @@
 
 int main()
 {
-  
+  int mtr[3][3];
+	int r,c,max[3],temp;
+	printf("enter the numbers in matrix in 3x3 setup:");
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+
+			scanf("%d",&mtr[r][c]);
+		}
+	}
+	printf("\n");
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			printf("%d ",mtr[r][c]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(r=0;r<=2;r++)
+	{
+		temp=mtr[r][0];
+		for(c=1;c<=2;c++)
+		{
+			if(mtr[r][c]>temp)
+			{
+				temp=mtr[r][c];
+			}
+		}
+		max[r]=temp;
+	}
+	printf("\n");
+	for(r=1;r<=3;r++)
+	{
+		
+		printf("max for %d row is= %d\n",r,max[r]);
+	}
+
   return 0;
 }
